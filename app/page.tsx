b2f0f1fea6e19d5e04382266509a60382e7cfb45
HusoNode <154274674+aksamlan@users.noreply.github.com>
2025-03-18 00:40:10 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import TimeLoopGame from "@/components/time-loop-game"
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <TimeLoopGame />
+    </main>
+  )
+}
+
